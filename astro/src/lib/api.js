141cfbd0705b18bb832409e7964f3e438d46c165
Juliane Cassidy <julianecassidy@gmail.com>
2023-12-26 23:21:32 -0800

    add styling to blog pages and start fix for home page sidebar alignment

--- astro/src/lib/api.js
+++ astro/src/lib/api.js
@@ -1,7 +1,7 @@
 import { useSanityClient } from "astro-sanity";
 
 export async function getAllPosts() {
-  const query = `*[_type == 'post']{"tagData": tags[]->{title},author -> {name},"mainImage": mainImage.asset->{url}, ...} | order(publishedAt desc)`;
+  const query = `*[_type == 'post']{...,"tagData": tags[]->{title},"author":author->name,"mainImage": mainImage.asset->url} | order(publishedAt desc)`;
   const data = await useSanityClient().fetch(query);
   return data;
 }
