2318b705337bbf3110b761e1bf4d1c8b01d69863
Juliane Cassidy <julianecassidy@gmail.com>
2023-12-28 10:15:16 -0800

    comment out index.png.ts

--- astro/src/pages/posts/[slug]/index.png.ts
+++ astro/src/pages/posts/[slug]/index.png.ts
@@ -1,39 +1,37 @@
-import type { APIRoute } from "astro";
-import { generateOgImageForPost } from "@utils/generateOgImages";
-import { slugifyStr } from "@utils/slugify";
-import { getAllPosts } from "lib/api";
-import { formatPostData } from "@utils/helpers.js";
+// import type { APIRoute } from "astro";
+// import { generateOgImageForPost } from "@utils/generateOgImages";
+// import { slugifyStr } from "@utils/slugify";
+// import { getAllPosts } from "lib/api";
+// import { formatPostData } from "@utils/helpers.js";
 
 
-export interface Post {
-  title: string,
-  slug: string,
-  author: string,
-  mainImage: string,
-  mainImageAlt: string,
-  tags: [],
-  tagData: [],
-  publishedAt: string,
-  featured: boolean,
-  body: string,
-  description: any,
-  _id: string,
-}
+// export interface Post {
+//   title: string,
+//   slug: string,
+//   author: string,
+//   mainImage: string,
+//   mainImageAlt: string,
+//   tags: [],
+//   tagData: [],
+//   publishedAt: string,
+//   featured: boolean,
+//   body: string,
+//   description: any,
+//   _id: string,
+// }
 
-export async function getStaticPaths() {
-  let posts = formatPostData(await getAllPosts());
+// export async function getStaticPaths() {
+//   let posts = formatPostData(await getAllPosts());
 
-  posts = ((p:Post[]) =>
-    p.filter((post:Post) => !post.mainImage)
-  );
+//   posts = posts.filter((post:Post) => !post.mainImage);
 
-  return posts.map((post:Post) => ({
-    params: { slug: slugifyStr(post.title) },
-    props: post,
-  }));
-}
+//   return posts.map((post:Post) => ({
+//     params: { slug: slugifyStr(post.title) },
+//     props: post,
+//   }));
+// }
 
-export const GET: APIRoute = async ({ props }) =>
-  new Response(await generateOgImageForPost(props as Post), {
-    headers: { "Content-Type": "image/png" },
-  });
+// export const GET: APIRoute = async ({ props }) =>
+//   new Response(await generateOgImageForPost(props as Post), {
+//     headers: { "Content-Type": "image/png" },
+//   });
